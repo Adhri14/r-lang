4e7f90be3e167df225bb13e0adceeb1259930c88
Adhri14 <adhri.adly@gmail.com>
2025-05-25 16:48:56 +0800

    add file determinan_matrix

---
+++ determinan_matrix.r
@@ -0,0 +1,8 @@
+# Definisikan matriks A dan B
+A <- matrix(c(3, 2, -1,
+              1, 6,  3,
+              2, -4, 0), nrow = 3, byrow = TRUE)
+B <- matrix(c(3,  1, -2,
+              5, -2, -3,
+              2,  2,  3), nrow = 3, byrow = TRUE)
+
