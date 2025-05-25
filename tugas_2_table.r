05850e24f75185d5145573d025cf730f25601363
Adhri14 <adhri.adly@gmail.com>
2025-05-25 16:04:19 +0800

    fixing

--- tugas_2_table.r
+++ tugas_2_table.r
@@ -1,6 +1,5 @@
-# Membaca data dari file Nilai_Komputer.txt
-# Pastikan file berada di working directory atau berikan path lengkapnya
-
+# Mengambil data dari file Nilai_Komputer.txt
+setwd("/mnt/mydrive/r-language/r-lang")
 data <- read.table("Nilai_Komputer.txt", header = TRUE, sep="\t", stringsAsFactors = FALSE)
 
 # Menampilkan data yang telah dibaca
